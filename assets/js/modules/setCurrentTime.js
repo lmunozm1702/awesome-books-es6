f9a0a842c95c31316d121bcb25465761aed54240
Luis Muñoz <lmunozm1702@gmail.com>
2022-10-30 20:04:08 -0300

    Refactor date & time using luxon module.
    Add luxon module (luxon.js) from https://github.com/moment/luxon/
    
    update setCurrentTime.js to use luxon module.

--- assets/js/modules/setCurrentTime.js
+++ assets/js/modules/setCurrentTime.js
@@ -1,16 +1,8 @@
+import { DateTime } from "./luxon.js";
+
 const setCurrentTime = () => {
-  const n = new Date();
-  const y = n.getFullYear();
-  const m = n.toLocaleString('default', { month: 'long' });
-  const d = n.getDate();
-
-  const h = n.getHours();
-  let mm = n.getMinutes();
-  if (mm < 10) { mm = `0${mm}`; }
-  let s = n.getSeconds();
-  if (s < 10) { s = `0${s}`; }
-
-  return (`${m} ${d}th ${y}, ${h}:${mm}:${s}`);
+  const newFormat = { ...DateTime.DATETIME_MED_WITH_SECONDS, month: 'long' };
+  return (DateTime.local().toLocaleString(newFormat));
 }
 
 export default setCurrentTime;
